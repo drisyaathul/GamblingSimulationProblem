d63d1143ded39fb354485f7081e309556c7120fe
drisyaathul <drisyak809@gmail.com>
2023-04-25 23:17:30 +0800

    Added: Stack of every day and Bet amount of every Game

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,5 +3,15 @@ package com.bridgelabz;
 public class GamblingSimulator {
     public static void main(String[] args) {
         System.out.println(" *** GAMBLING SIMULATION PROBLEM *** ");
+        /*
+            * As a Gambler, would start with a stake of $100 every day and
+            bet $1 every game.
+            * Initializing Stack of Every day and Bet Amount
+         */
+        int stackEveryDay = 100;
+        int betEveyGame = 1;
+
+        System.out.println("Stack of Every Day = "+stackEveryDay+ "$");
+        System.out.println("Bet Amount Every Game = "+betEveyGame+ "$");
     }
 }
