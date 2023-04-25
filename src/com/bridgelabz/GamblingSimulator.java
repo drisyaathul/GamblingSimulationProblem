4d68652833aee574fe7077c2c88c65b2ba8947cc
drisyaathul <drisyak809@gmail.com>
2023-04-25 23:52:35 +0800

    Added : A Gambler make  bet either win or loose

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -3,17 +3,38 @@ package com.bridgelabz;
 import java.util.Random;
 
 public class GamblingSimulator {
+    /*
+      * As a Gambler, would start with a stake of $100 every day and
+        bet $1 every game(UC1).
+      * Initializing Stack of Every day and Bet Amount
+    */
+    static int stackEveryDay = 100;
+    static int betEveyGame = 1;
     public static void main(String[] args) {
         System.out.println(" *** GAMBLING SIMULATION PROBLEM *** ");
-        /*
-            * As a Gambler, would start with a stake of $100 every day and
-            bet $1 every game.
-            * Initializing Stack of Every day and Bet Amount
-         */
-        int stackEveryDay = 100;
-        int betEveyGame = 1;
 
         System.out.println("Stack of Every Day = "+stackEveryDay+ "$");
         System.out.println("Bet Amount Every Game = "+betEveyGame+ "$");
+        /*
+          * Gambler make $1 bet either win or loose $1 (UC2).
+          * Using Random to find the Bet wins or lose.
+         */
+        Random random =new Random();
+        int bet = random.nextInt(2);
+        if (bet == 1) {
+            System.out.println("Gambler Bet WINS");
+            /*
+               When the Bet wins Bet amount is added to Stack of Everyday
+             */
+            stackEveryDay += betEveyGame;
+            System.out.println("Final Stack when Bet Wins = "+stackEveryDay+"$");
+        }else {
+            System.out.println("Gambler Bet Lose");
+            /*
+               When the Bet loose Stack of Every day is deducted by bet amount
+             */
+            stackEveryDay -= betEveyGame;
+            System.out.println("Final Stack when Bet Lose = "+stackEveryDay+"$");
+        }
     }
 }
