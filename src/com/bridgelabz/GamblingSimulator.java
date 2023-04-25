c0d4cb88dc14b0254ae2dda999590e86a5c2b9e7
drisyaathul <drisyak809@gmail.com>
2023-04-25 11:50:44 +0800

    Gambling Simulation Problem

---
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -0,0 +1,7 @@
+package com.bridgelabz;
+
+public class GamblingSimulator {
+    public static void main(String[] args) {
+        System.out.println(" *** GAMBLING SIMULATION PROBLEM *** ");
+    }
+}
