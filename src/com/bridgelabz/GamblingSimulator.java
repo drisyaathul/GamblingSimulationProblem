c875d0ad7cbab57ce94dba5243c41fc2c49cd23d
drisyaathul <drisyak809@gmail.com>
2023-04-26 16:09:25 +0800

    added: Total Amount of win or loss after 20 days

--- src/com/bridgelabz/GamblingSimulator.java
+++ src/com/bridgelabz/GamblingSimulator.java
@@ -4,10 +4,10 @@ import java.util.Random;
 
 public class GamblingSimulator {
     /*
-      * As a Gambler, would start with a stake of $100 every day and
-        bet $1 every game(UC1).
-      * Initializing Stack of Every day and Bet Amount
-    */
+     * As a Gambler, would start with a stake of $100 every day and
+       bet $1 every game(UC1).
+     * Initializing Stack of Every day and Bet Amount
+   */
     static int stackEveryDay = 100;
     static int betEveyGame = 1;
 
@@ -21,31 +21,48 @@ public class GamblingSimulator {
          */
         int stackWon = 150;
         int stackLost = 50;
+        int totalAmount=0;
+        /*
+           Playing the game for 20 days (UC4)
+         */
+        for (int days = 1; days <= 20; days++) {
         /*
            Gambler if won or lost 50% of the stake, would resign for the day (UC3)
          */
-        while (stackEveryDay > stackLost && stackEveryDay < stackWon) {
+            while (stackEveryDay > stackLost && stackEveryDay < stackWon) {
+                /*
+                 * Gambler make $1 bet either win or loose $1 (UC2).
+                 * Using Random to find the Bet wins or lose.
+                 */
+                Random random = new Random();
+                int bet = random.nextInt(2);
+                /*
+                   When the Bet wins Bet amount is added to Stack of Everyday
+                */
+                if (bet == 1) {
+                    stackEveryDay += betEveyGame;
+                } else {
+                    /*
+                       When the Bet loose Stack of Every day is deducted by bet amount
+                    */
+                    stackEveryDay -= betEveyGame;
+                }
+            }
             /*
-             * Gambler make $1 bet either win or loose $1 (UC2).
-             * Using Random to find the Bet wins or lose.
+              * if the Stack ot Every day is equal to 150$ else 50$
+              * total amount is total stack of every day
              */
-            Random random = new Random();
-            int bet = random.nextInt(2);
-            if (bet == 1) {
-                System.out.println("Gambler Bet WINS");
-            /*
-               When the Bet wins Bet amount is added to Stack of Everyday
-             */
-                stackEveryDay += betEveyGame;
-                System.out.println("Final Stack when Bet Wins = " + stackEveryDay + "$");
-            } else {
-                System.out.println("Gambler Bet Lose");
-            /*
-               When the Bet loose Stack of Every day is deducted by bet amount
-             */
-                stackEveryDay -= betEveyGame;
-                System.out.println("Final Stack when Bet Lose = " + stackEveryDay + "$");
+            if(stackEveryDay == stackWon) {
+                System.out.println("Player Has Won Gambler For Day "+days);
+                totalAmount += stackEveryDay;
+            }else {
+                System.out.println("Player Has Loss Gambler For Day "+days);
+                totalAmount -= stackEveryDay;
             }
         }
+        /*
+           After 20 days of playing every day would like to know the total amount won or lost (UC4)
+         */
+        System.out.println("Total Win Price: "+totalAmount + "\n");
     }
 }
